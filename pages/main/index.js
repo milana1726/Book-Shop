ccecbfd56ce49f69107133426b810fac61aae8c3
Melnychuk <IMelnychuk@poland.luxoft.com>
2022-05-24 13:57:37 +0200

    feat: implement removing from cart

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,5 +1,5 @@
 let cartTemp = [];
-let order = '';
+// let order = '';
 let totalPrice = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -39,30 +39,50 @@ document.addEventListener('DOMContentLoaded', () => {
   const main_container = document.createElement('div');
   main_container.classList.add('main_container');
 
+  //book catalog
   const book_catalog = document.createElement('div');
   book_catalog.classList.add('book_catalog');
 
+  //cart
   const cart = document.createElement('div');
   cart.classList.add('cart');
 
-  main.append(main_container);
+  const cart_name = document.createElement('h2');
+  cart_name.classList.add('cart_name');
+  cart_name.innerHTML = 'Cart';
+
+   //cart items 
+   const cart_list = document.createElement('div');
+   cart_list.classList.add('cart_list');
+
+   //total cart section
+   const total = document.createElement('div');
+   total.classList.add('total');
+   total.innerHTML = `
+     <hr>
+     <p>Total: <b><span class="total_count">${totalPrice}</span></b></p>
+     <button disabled id="button_confirm" type="button" 
+       onClick="location.href='../delivery/index.html'">Confirm order</button>`;
+
+  cart.append(cart_name, cart_list, total);
+  
   main_container.append(book_catalog, cart);
+  main.append(main_container);
 
   //modal
   const modalContainer = document.createElement('div');
   modalContainer.classList.add('modal_container');
 
-  //total cart section
-  const total = document.createElement('div');
-  total.classList.add('total');
-  total.innerHTML = `
-    <hr>
-    <p class="total_prise">Total: <b><span class="total_count">${totalPrice}</span></b></p>
-    <button class="button_confirm" type="button" 
-      onClick="location.href='../delivery/index.html'">Confirm order</button>`;
-  cart.append(total);
+  //footer
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
 
-  wrapper.append(header, main, modalContainer);
+  const footer_text = document.createElement('h3');
+  footer_text.innerHTML = '© 2022 Book Shop - Liudmyla Melnychuk';
+
+  footer.append(footer_text);
+
+  wrapper.append(header, main, modalContainer, footer);
 
   fetch('./book.json')
           .then(response => {
@@ -73,16 +93,16 @@ document.addEventListener('DOMContentLoaded', () => {
             return books;
           })
           .then(data => {
-            bookInfo(data);
+            bookCatalog(data);
           });
 
       fragment.appendChild(wrapper);
       body.appendChild(fragment);
 });
 
-function bookInfo(data) {
+function bookCatalog(data) {
   let output = '';
-  
+  let name = `<h2 class = "catalog_name">Catalog</h2>`;
     for (let item of data) {
         output += `
         <div draggable="true" class="book_card">
@@ -97,7 +117,7 @@ function bookInfo(data) {
         </div>`
   }
   
-  document.querySelector('.book_catalog').innerHTML = output;
+  document.querySelector('.book_catalog').innerHTML = name + output;
     
   const moreButton = document.querySelectorAll('.button_more');
   moreButton.forEach((button, index) => {
@@ -137,27 +157,33 @@ function addToCart(data, index) {
   cartTemp.push(data[index]);
   totalPrice += data[index].price;
   localStorage.setItem('totalPrice', totalPrice);
-  cartInfo(cartTemp, index);
-  console.log(totalPrice);
+  updateCart(cartTemp);
+
+  let btn_confirm = document.getElementById('button_confirm');
+  btn_confirm.disabled = false;
+  
   return totalPrice;
 }
 
-function cartInfo(cartTemp, index) {
+function updateCart(cartTemp) {
   if (cartTemp.length == 4) {
     alert('Your cart is full!');
   } else {
-    let output = document.querySelector('.cart');
-    output.innerHTML += `
+    let output = '';
+    for (let item of cartTemp) {
+        output += `
         <div class="book_card">
-          <img src="${cartTemp[index].imageLink}" alt="image_book">
+          <img src="${item.imageLink}" alt="image_book">
           <div class="about_book">
-          <p class="title">${cartTemp[index].title}</p>
-            <p class="author">${cartTemp[index].author}</p>
+          <p class="title">${item.title}</p>
+            <p class="author">${item.author}</p>
             <button class="button_delete" type="button">
               <img id="icon_delete" src="../../assets/icons/delete_icon.svg" alt="delete_icon"/></button>
           </div>
         </div>`
-    }
+  }
+
+    document.querySelector('.cart_list').innerHTML = output;
     document.querySelector('.total_count').innerHTML = `${totalPrice}$`;
 
     const removeButton = document.querySelectorAll('.button_delete');
@@ -166,35 +192,22 @@ function cartInfo(cartTemp, index) {
             removeFromCart(index);
         }
     }) 
+  }
 }
 
-// function removeFromCart(index) {
-//   if (totalPrice !== 0){
-//     totalPrice -= cartTemp[index].price;
-//       document.querySelector('.total_count').innerHTML = `${totalPrice}$`;
-//   }
-//   for (let i = 0; i < cartTemp[index]; i += 1) {
-//     document.querySelectorAll('.button_delete')[i].addEventListener('click', (e) => {
-//         e.target.parentElement.remove();
-//   // cartTemp.splice(cartTemp[index], 1);
+function removeFromCart(index) {
+  if (totalPrice !== 0){
+    totalPrice -= cartTemp[index].price;
+    document.querySelector('.total_count').innerHTML = `${totalPrice}$`;
+  }
 
-//   //console.log(cartTemp);
-//     });
-    
-//   }
+  cartTemp.splice(cartTemp[index], 1);
+  updateCart(cartTemp);
+  
+  return cartTemp, totalPrice;
+}
 
-//   if (cartTemp.length === 1) {
-//       const clearCart = document.createElement('div');
-//       clearCart.classList.add('empty_cart');
-//       clearCart.innerHTML = `
-//       <div class="book_cart">Your cart is empty</div>
-//       `
-//       cart.append(clearCart);
-//       totalPrice = 0;
-//     } else {
-      
-//   }
+  
 
-//   return cartTemp, totalPrice;
 
-// }
+  
